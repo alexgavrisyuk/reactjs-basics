250b2e9b3d425d1feeeb6140be946630ea91a3eb
Alex Gavrisyuk <ogavrisyuk@iszolea.com>
2018-11-26 14:04:17 +0200

    Added multiple components

---
+++ src/app/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>All right protected</p>
+                    <footer class="blockquote-footer">By <cite title="Source Title">Alex Havrysyuk</cite></footer>
+                </blockquote>
+            </div>
+        );
+    };
+}
---
+++ src/app/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <big class="text-muted">> This is Home Component. Yeah !! </big>
+            </div>
+        );
+    }
+}
